30b6c4c6919c761310440ec8cc2a155730b4158e
Jose Segovia <jose.segovia.r@gmail.com>
2024-08-26 00:39:40 -0400

    fix test

--- src/pages/apps/user/view/[tab].tsx
+++ src/pages/apps/user/view/[tab].tsx
@@ -1,42 +1,35 @@
 // ** Next Import
-import { GetStaticProps, GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from 'next/types'
+import { GetStaticProps, GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next/types";
 
 // ** Third Party Imports
-import axios from 'axios'
+import axios from "axios";
 
 // ** Types
-import { InvoiceType } from 'src/types/apps/invoiceTypes'
+import { Attendance } from "src/services/graphql/types";
 
 // ** Demo Components Imports
-import UserViewPage from 'src/views/apps/user/view/UserViewPage'
+import UserViewPage from "src/views/apps/user/view/UserViewPage";
 
-const UserView = ({ tab, invoiceData }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return <UserViewPage tab={tab} invoiceData={invoiceData} />
-}
+const UserView = ({ tab, attendanceData }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return <UserViewPage tab={tab} attendanceData={attendanceData} />;
+};
 
 export const getStaticPaths: GetStaticPaths = () => {
   return {
-    paths: [
-      { params: { tab: 'overview' } },
-      { params: { tab: 'security' } },
-      { params: { tab: 'billing-plan' } },
-      { params: { tab: 'notification' } },
-      { params: { tab: 'connection' } }
-    ],
-    fallback: false
-  }
-}
+    paths: [{ params: { tab: "overview" } }, { params: { tab: "security" } }, { params: { tab: "billing-plan" } }, { params: { tab: "notification" } }, { params: { tab: "connection" } }],
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
-  const res = await axios.get('/apps/invoice/invoices')
-  const invoiceData: InvoiceType[] = res.data.allData
+  const attendanceData: Attendance[] = [];
 
   return {
     props: {
-      invoiceData,
-      tab: params?.tab
-    }
-  }
-}
+      attendanceData,
+      tab: params?.tab,
+    },
+  };
+};
 
-export default UserView
+export default UserView;
--- src/views/apps/user/view/UserViewPage.tsx
+++ src/views/apps/user/view/UserViewPage.tsx
@@ -1,29 +1,29 @@
 // ** MUI Imports
-import Grid from '@mui/material/Grid'
+import Grid from "@mui/material/Grid";
 
 // ** Types
-import { InvoiceType } from 'src/types/apps/invoiceTypes'
+import { Attendance } from "src/services/graphql/types";
 
 // ** Demo Components Imports
-import UserViewLeft from 'src/views/apps/user/view/UserViewLeft'
-import UserViewRight from 'src/views/apps/user/view/UserViewRight'
+import UserViewLeft from "src/views/apps/user/view/UserViewLeft";
+import UserViewRight from "src/views/apps/user/view/UserViewRight";
 
 type Props = {
-  tab: string
-  invoiceData: InvoiceType[]
-}
+  tab: string;
+  attendanceData: Attendance[];
+};
 
-const UserView = ({ tab, invoiceData }: Props) => {
+const UserView = ({ tab, attendanceData }: Props) => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12} md={5} lg={4}>
         <UserViewLeft />
       </Grid>
       <Grid item xs={12} md={7} lg={8}>
-        <UserViewRight tab={tab} invoiceData={invoiceData} />
+        <UserViewRight tab={tab} attendanceData={attendanceData} />
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default UserView
+export default UserView;
